900622a05db5084dfa1afbb45f146fea86d2462c
erick-gulyan <97641133+erick-gulyan@users.noreply.github.com>
2022-01-26 15:45:48 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,9 +3,11 @@ import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
-
+    //this denoted that we are going to run a JUnit test
     public void addition(){
         assertEquals(2,1+1);
         //assertEquals(expected,actual);
+        //checks to see if expected:2 is equal to actual :1+1=2.
+         //Should be true
     }
 }
