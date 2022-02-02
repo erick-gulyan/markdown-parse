6821a4855a313ed59e149f8763c2f94c75185c80
erick-gulyan <97641133+erick-gulyan@users.noreply.github.com>
2022-02-02 15:54:39 -0800

    Added test for images
    added if statement which checks for "!" before the [

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,10 +19,14 @@ public class MarkdownParse {
             if( nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
                 break;
             }
-            if( nextCloseBracket + 1 == openParen) {
+            if(markdown.charAt(nextOpenBracket-1) == '!'){
+                
+            }
+            else if( nextCloseBracket + 1 == openParen) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
+            
         }
         return toReturn;
     }
